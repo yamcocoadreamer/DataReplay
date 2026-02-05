2dd578621642df5e52a633c0674aa77b9059aa20
Marine Airault <yamcocoadreamer@gmail.com>
2026-02-05 23:14:41 +0100

    Initial Unreal project setup

---
+++ Source/DataReplay/DataReplay.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DataReplay.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DataReplay, "DataReplay" );
